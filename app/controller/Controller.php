6c7f490cf36a2e7b2852ad627c12d159378a4e56
Sergey Belozyorov <belozyorovsergey@yandex.ru>
2016-07-14 17:48:41 +0300

    Include bootstrap and make Helper

--- app/controller/Controller.php
+++ app/controller/Controller.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 class Controller{
 	const VIEW_PATH = 'app/view/';
 	const LAYOUT = 'app/view/layout.php';
@@ -11,6 +10,19 @@ class Controller{
 		$this->viewBody = self::VIEW_PATH . $controller . '/' . $action . '.php';
 
 		require_once(self::LAYOUT);
+	}
+}
+
+class Helper{
+	public static function url($controller, $action, $params = null){
+		$url = "/index.php?controller={$controller}&action={$action}";
+		if($params === null) return $url;
+
+		foreach($params as $key => $value){
+			$url .= "&$key=$value";
+		}
+
+		return url;
 	}
 }
 
--- app/view/layout.php
+++ app/view/layout.php
@@ -1,5 +1,25 @@
-<h1>It's layout</h1>
-<h2>title - <?php echo $this->viewTitle; ?></h2>
-<?php 
-	require_once($this->viewBody);
-?>
+<!doctype html>
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?php echo $this->viewTitle; ?></title>
+  <link rel="stylesheet" type="text/css" href="../../websource/css/bootstrap.min.css">
+  <script type="text/javascript" src="../../websource/js/jquery-1.11.3.js"></script>
+  <script type="text/javascript" src="../../websource/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="<?php echo Helper::url('home', 'index'); ?>">
+					Тестовое задание
+				</a>
+			</div>
+		</div>
+	</div>
+
+	<?php require_once($this->viewBody); ?>
+</body>
+</html>

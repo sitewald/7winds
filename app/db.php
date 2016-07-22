e345c4290c5af1c2fe9bef0adf7317b4a5b71aa8
Sergey Belozyorov <belozyorovsergey@yandex.ru>
2016-07-22 12:39:31 +0300

    change db error message page

--- app/db.php
+++ app/db.php
@@ -5,7 +5,8 @@ class db{
 	const USER = 'root';
 	const PASSWORD = '';
 	const DBNAME = '7winds';
-	const CONNECTION_ERROR = '<h1>Проверьте настройки подключения к БД в app/db.php</h1>';
+	const CONNECTION_ERROR = 
+	'<html><head><meta charset="utf-8"></head><body><h1>Проверьте настройки подключения к БД в app/db.php</h1><a href="/"><< На главную</a></body></html>';
 }
 
 ?>
